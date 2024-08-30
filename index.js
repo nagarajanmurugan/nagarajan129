5824d2a519ff75f5435ee844cd8a199f11b260f9
nagarajan129 <nagarajanmnagarajan77@gmail.com>
2024-08-30 10:11:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function toUpperCaseString(inputString) {
+    return inputString.toUpperCase();
+}
+
+// Example usage:
+const originalString = "hello, world!";
+const upperCaseString = toUpperCaseString(originalString);
+
+console.log(upperCaseString); // Outputs: "HELLO, WORLD!"
+
+module.exports = toUpperCaseString;
